094d10e226d523527ae81f52ed96f8baae54ab72
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-03-02 12:10:58 +0530

    Initial commit

---
+++ app/src/main/java/com/msomu/handysms/MainActivity.java
@@ -0,0 +1,83 @@
+package com.msomu.handysms;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        readAllMessage();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public List<SmsDataClass> readAllMessage(){
+        List<SmsDataClass> sms = new ArrayList<SmsDataClass>();
+
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        Cursor cur = getContentResolver().query(uriSMSURI, null, null, null, null);
+        while (cur.moveToNext())
+        {
+            SmsDataClass smsDataClass = new SmsDataClass();
+            //String address = cur.getString(cur.getColumnIndex("address"));
+            smsDataClass.setBody(cur.getString(cur.getColumnIndexOrThrow("body")));
+            smsDataClass.setAddress(cur.getString(cur.getColumnIndexOrThrow("address")));
+            //sms.add(body);
+            //Log.d("Cursor Body",body);
+            Log.d(" Sms Data ",smsDataClass.getAddress()+" : "+smsDataClass.getBody());
+            sms.add(smsDataClass);
+        }
+        Log.d("MainActivity","Total sms read"+sms.size());
+        cur.close();
+        return sms;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/msomu/handysms/SmsDataClass.java
@@ -0,0 +1,52 @@
+package com.msomu.handysms;
+
+/**
+ * Created by msomu on 02/03/16.
+ */
+public class SmsDataClass {
+    private String address;
+    private String body;
+    private String date;
+    private String type;
+    private String id;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
